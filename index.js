3eb11cd2e143a285366c2a60fdf6359383a4579f
Ajay Sehwal <110731336+ajaysehwal@users.noreply.github.com>
2023-09-27 15:33:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,21 +12,30 @@ const path = require("path");
 const xlsx = require("xlsx");
 const fs = require("fs-extra");
 app.use(bodyParser.json());
-const corsOptions = {
-  origin: 'https://oohrerpsoftware.vercel.app',
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",};
+pp.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://oohrerpsoftware.vercel.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
 
-  // app.use(function (req, res, next) {
-  //   //Enabling CORS
-  //   res.header("Access-Control-Allow-Origin", "https://oohrerpsoftware.vercel.app");
-  //   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  //     next();
-  //   });
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.use(cors());
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/images/studentfiles", express.static("images/studentfiles"));
 app.use("/images/teachersfiles", express.static("images/teachersfiles"));
