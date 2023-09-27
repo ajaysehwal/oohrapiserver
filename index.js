a352fc3deaea0fc459047220373c2c31e929eac3
Ajay Sehwal <110731336+ajaysehwal@users.noreply.github.com>
2023-09-27 15:46:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,30 +12,15 @@ const path = require("path");
 const xlsx = require("xlsx");
 const fs = require("fs-extra");
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://oohrerpsoftware.vercel.app"
-  );
-  // res.setHeader(
-  //   "Access-Control-Allow-Methods",
-  //   "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  // );
-  // res.setHeader(
-  //   "Access-Control-Allow-Headers",
-  //   "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  // );
-  // res.setHeader("Access-Control-Allow-Credentials", true);
-  // res.setHeader("Access-Control-Allow-Private-Network", true);
-  // //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  // res.setHeader("Access-Control-Max-Age", 7200);
 
-  next();
-});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://oohrerpsoftware.vercel.app',
+  methods: 'GET,HEAD,OPTIONS,POST,PUT', // Add the allowed methods
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'], // Add the allowed headers
+}));
 
 app.use("/images/studentfiles", express.static("images/studentfiles"));
 app.use("/images/teachersfiles", express.static("images/teachersfiles"));
